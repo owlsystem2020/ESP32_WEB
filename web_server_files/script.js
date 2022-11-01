9ee790112333426c9d767361e692b9bc35fe49c8
Serge <owl.system2020@gmail.com>
2022-11-01 20:38:57 +0300

    JavaScript optimisation

--- web_server_files/script.js
+++ web_server_files/script.js
@@ -1,33 +1,3 @@
-function ajaxAction(str) 
-{  if (str == "") 
-    {    document.getElementById("txtHint").innerHTML = "";    return;  }  
-    const xhttp = new XMLHttpRequest();  xhttp.onload = function()
-    
-    {    document.getElementById("txtHint").innerHTML = this.responseText;  };  
-    xhttp.open("GET", "act?mode="+str);  xhttp.send();
-}
-
-function loadData()
-{
-    // {    document.getElementById("ec").innerHTML = this.responseText;  };  
-    // xhttp.open("GET", "act?ec=1");  xhttp.send();
-
-    {    document.getElementById("nLevel").innerHTML = this.responseText;  };  
-    xhttp.open("GET", "act?nlevel=1");  xhttp.send();
-
-    {    document.getElementById("relayState").innerHTML = this.responseText;  };  
-    xhttp.open("GET", "act?relaystate=1");  xhttp.send();
-
-    {    document.getElementById("temp").innerHTML = this.responseText;  };  
-    xhttp.open("GET", "act?temp=1");  xhttp.send();
-
-    {    document.getElementById("time").innerHTML = this.responseText;  };  
-    xhttp.open("GET", "act?time=1");  xhttp.send();
-
-    {    document.getElementById("co2").innerHTML = this.responseText;  };  
-    xhttp.open("GET", "act?co2=1");  xhttp.send();
-
-}
 
 function loadSettings()
 {
@@ -47,6 +17,7 @@ function loadSettings()
                 document.getElementById("mac").innerHTML = obj.mac;
                 document.getElementById("mode").innerHTML = obj.mode;
                 document.getElementsByName("wifi_ssid")[0].placeholder=obj.wifi_ssid;
+                document.getElementsByName("r1_time_on")[0].value="06:00";
             } 
             else 
             {
@@ -74,41 +45,6 @@ function loadSettings()
     httpRequest.send();
 }
 
-
-window.addEventListener( "load", function () {
-    function sendData() {
-      const XHR = new XMLHttpRequest();
-  
-      // Bind the FormData object and the form element
-      const FD = new FormData( form );
-      const queryString = new URLSearchParams(FD).toString()
-  
-      // Define what happens on successful data submission
-      XHR.addEventListener( "load", function(event) {
-        alert( event.target.responseText );
-      } );
-  
-      // Define what happens in case of error
-      XHR.addEventListener( "error", function( event ) {
-        alert( 'Oops! Something went wrong.' );
-      } );
-  
-      // Set up our request
-      XHR.open( "GET", "act?" + queryString);
-  
-      // The data sent is what the user provided in the form
-      XHR.send( FD );
-    }
-  
-    // Access the form element...
-    const form = document.getElementById( "wifi_ssid_form" );
-    //const relay1Form = document.getElementById( "relay1_form" );
-  
-    // ...and take over its submit event.
-    form.addEventListener( "submit", sendData);
-    //relay1Form.addEventListener( "submit", sendData);
-
-  } );
 
   window.addEventListener( "load", function () {
     function sendData(formName) {
@@ -138,9 +74,13 @@ window.addEventListener( "load", function () {
   
     // Access the form element...
     const relay1Form = document.getElementById( "r1_form" );
+    const wifiForm = document.getElementById( "wifi_ssid_form" );
   
     // ...and take over its submit event.
     relay1Form.addEventListener( "change", function () { sendData(relay1Form); });
+    // ...and take over its submit event.
+    wifiForm.addEventListener( "submit", function () { sendData(wifiForm); });
+
   } );
 
 
