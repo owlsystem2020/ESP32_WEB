ef495be761311b1a48bb47f220ec4cf8e98f90dc
Serge <owl.system2020@gmail.com>
2022-11-04 09:01:30 +0300

    Added TZ CSV and changed FS image size in script

--- mk_fs_write_fs.py
+++ mk_fs_write_fs.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 #
 # creation of file system image
-python3 spiffsgen.py --page-size 256 --block-size 4096 32768 web_server_files/ fs.bin
+python3 spiffsgen.py --page-size 256 --block-size 4096 65536 web_server_files/ fs.bin
 # load fs to ESP32
 python3 esptool-master/esptool.py --chip auto --port /dev/cu.usbserial-14120 --baud 115200 write_flash -z 0x110000 fs.bin
 
